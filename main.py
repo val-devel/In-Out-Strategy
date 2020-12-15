d350b0d8aeb754126719437d9e25292c4fabcd71
val-devel <76076847+val-devel@users.noreply.github.com>
2020-12-15 18:57:25 -0500

    30.2/17.9-1.897
    T Smith.
    Adjusting the tradeout scheduling and using hourly rather than daily resolution for our signal instruments, I've managed to reduce drawdown.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+"""
+DUAL MOMENTUM-IN OUT v2 by Vladimir
 https://www.quantconnect.com/forum/discussion/9597/the-in-amp-out-strategy-continued-from-quantopian/p3/comment-28146
 
-"""
-Based on 'In & Out' strategy by Peter Guenther 4 Oct 2020
-expanded/inspired by Tentor Testivis, Dan Whitnable, Vladimir, and Thomas Chang.
+inspired by Peter Guenther, Tentor Testivis, Dan Whitnable, Thomas Chang and T Smith.
 
 """
 import numpy as np
@@ -13,57 +13,61 @@ class DualMomentumInOut(QCAlgorithm):
 
         self.SetStartDate(2008, 1, 1)
         # self.SetEndDate(2020, 11, 27)
-        self.cap = 100000
+        self.cap = 100000        
+        
+        self.STK1 = self.AddEquity('QQQ', Resolution.Hour).Symbol
+        self.STK2 = self.AddEquity('FDN', Resolution.Hour).Symbol
+        self.BND1 = self.AddEquity('TLT', Resolution.Hour).Symbol
+        self.BND2 = self.AddEquity('TLH', Resolution.Hour).Symbol
+        self.ASSETS = [self.STK1, self.STK2, self.BND1, self.BND2]
 
-        self.BND1 = self.AddEquity('TLT', Resolution.Minute).Symbol
-        self.BND2 = self.AddEquity('TLH', Resolution.Minute).Symbol
-        self.STK1 = self.AddEquity('QQQ', Resolution.Minute).Symbol
-        self.STK2 = self.AddEquity('FDN', Resolution.Minute).Symbol
-
-        self.MKT = self.AddEquity('SPY', Resolution.Daily).Symbol  
-        self.XLI = self.AddEquity('XLI', Resolution.Daily).Symbol 
-        self.XLU = self.AddEquity('XLU', Resolution.Daily).Symbol 
-        self.SLV = self.AddEquity('SLV', Resolution.Daily).Symbol 
-        self.GLD = self.AddEquity('GLD', Resolution.Daily).Symbol
-        self.FXA = self.AddEquity('FXA', Resolution.Daily).Symbol
-        self.FXF = self.AddEquity('FXF', Resolution.Daily).Symbol
-        self.DBB = self.AddEquity('DBB', Resolution.Daily).Symbol  
-        self.IGE = self.AddEquity('IGE', Resolution.Daily).Symbol
-        self.SHY = self.AddEquity('SHY', Resolution.Daily).Symbol  
-        self.UUP = self.AddEquity('UUP', Resolution.Daily).Symbol 
+        self.MKT = self.AddEquity('SPY', Resolution.Hour).Symbol  
+        self.XLI = self.AddEquity('XLI', Resolution.Hour).Symbol 
+        self.XLU = self.AddEquity('XLU', Resolution.Hour).Symbol 
+        self.SLV = self.AddEquity('SLV', Resolution.Hour).Symbol 
+        self.GLD = self.AddEquity('GLD', Resolution.Hour).Symbol 
+        self.FXA = self.AddEquity('FXA', Resolution.Hour).Symbol
+        self.FXF = self.AddEquity('FXF', Resolution.Hour).Symbol
+        self.DBB = self.AddEquity('DBB', Resolution.Hour).Symbol
+        self.UUP = self.AddEquity('UUP', Resolution.Hour).Symbol          
+        self.IGE = self.AddEquity('IGE', Resolution.Hour).Symbol
+        self.SHY = self.AddEquity('SHY', Resolution.Hour).Symbol        
 
         self.FORPAIRS = [self.XLI, self.XLU, self.SLV, self.GLD, self.FXA, self.FXF]
         self.SIGNALS  = [self.XLI, self.DBB, self.IGE, self.SHY, self.UUP]
-        self.pairlist = ['S_G', 'I_U', 'A_F']
+        self.PAIR_LIST = ['S_G', 'I_U', 'A_F']
         
         self.INI_WAIT_DAYS = 15
-        self.mom = 126
-        self.excl = 5
-        
-        self.BNDselect = self.BND1
-        self.STKselect = self.STK1
-        self.HLD_OUT = {self.BNDselect: 1}
-        self.HLD_IN = {self.STKselect: 1}
+        self.SHIFT = 55
+        self.MEAN = 11
+        self.RET = 126
+        self.EXCL = 5
+        self.leveragePercentage = 101
+        self.selected_bond = self.BND1
+        self.selected_stock = self.STK1
+        self.init = 0
         
         self.bull = 1 
         self.count = 0 
         self.outday = 0
+        self.in_stock = 0
         self.spy = []
         self.wait_days = self.INI_WAIT_DAYS
+        self.wt = {}
+        self.real_wt = {}
         self.SetWarmUp(timedelta(126))
 
-        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.AfterMarketOpen('SPY', 1),
+        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.AfterMarketOpen('SPY', 100),
             self.calculate_signal)
 
-        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.AfterMarketOpen('SPY', 120),
-            self.rebalance_when_out_of_the_market)
-
-        self.Schedule.On(self.DateRules.WeekEnd(), self.TimeRules.AfterMarketOpen('SPY', 121),
-            self.rebalance_when_in_the_market)
+        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.Every(TimeSpan.FromMinutes(15)),
+            self.trade_out)
+            
+        self.Schedule.On(self.DateRules.WeekEnd(), self.TimeRules.AfterMarketOpen('SPY', 120),
+            self.trade_in)    
             
         self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.BeforeMarketClose('SPY', 0), 
-            self.record_vars)  
-            
+            self.record_vars)            
             
         symbols = self.SIGNALS + [self.MKT] + self.FORPAIRS
         for symbol in symbols:
@@ -86,10 +90,10 @@ class DualMomentumInOut(QCAlgorithm):
         
         
     def update_history_shift(self):
-        self.history_shift_mean = self.history.shift(55).rolling(11).mean()    
+        self.history_shift_mean = self.history.shift(self.SHIFT).rolling(self.MEAN).mean()    
             
    
-    def Returns(self, symbol, period, excl):
+    def returns(self, symbol, period, excl):
         prices = self.History(symbol, TimeSpan.FromDays(period + excl), Resolution.Daily).close
         return prices[-excl] / prices[0]
         
@@ -108,16 +112,16 @@ class DualMomentumInOut(QCAlgorithm):
         self.wait_days = int(
             max(0.50 * self.wait_days,
                 self.INI_WAIT_DAYS * max(1,
-                                         np.where((mom[self.GLD].iloc[-1]>0) & (mom[self.SLV].iloc[-1]<0) & (mom[self.SLV].iloc[-2]>0), self.INI_WAIT_DAYS, 1),
-                                         np.where((mom[self.XLU].iloc[-1]>0) & (mom[self.XLI].iloc[-1]<0) & (mom[self.XLI].iloc[-2]>0), self.INI_WAIT_DAYS, 1),
-                                         np.where((mom[self.FXF].iloc[-1]>0) & (mom[self.FXA].iloc[-1]<0) & (mom[self.FXA].iloc[-2]>0), self.INI_WAIT_DAYS, 1)
-                                         ))
-        )
+                     np.where((mom[self.GLD].iloc[-1]>0) & (mom[self.SLV].iloc[-1]<0) & (mom[self.SLV].iloc[-2]>0), self.INI_WAIT_DAYS, 1),
+                     np.where((mom[self.XLU].iloc[-1]>0) & (mom[self.XLI].iloc[-1]<0) & (mom[self.XLI].iloc[-2]>0), self.INI_WAIT_DAYS, 1),
+                     np.where((mom[self.FXF].iloc[-1]>0) & (mom[self.FXA].iloc[-1]<0) & (mom[self.FXA].iloc[-2]>0), self.INI_WAIT_DAYS, 1)
+                     )))
+                     
         adjwaitdays = min(60, self.wait_days)
 
         # self.Debug('{}'.format(self.wait_days))
 
-        if (extreme[self.SIGNALS + self.pairlist]).any():
+        if (extreme[self.SIGNALS + self.PAIR_LIST]).any():
             self.bull = False
             self.outday = self.count
             
@@ -127,60 +131,48 @@ class DualMomentumInOut(QCAlgorithm):
         self.count += 1
 
         self.Plot("In Out", "in_market", int(self.bull))
-        self.Plot("In Out", "num_out_signals", extreme[self.SIGNALS + self.pairlist].sum())
+        self.Plot("In Out", "num_out_signals", extreme[self.SIGNALS + self.PAIR_LIST].sum())
         self.Plot("Wait Days", "waitdays", adjwaitdays)
 
-        if self.Returns(self.BND1, self.mom, self.excl) < self.Returns(self.BND2, self.mom, self.excl):
-            self.BNDselect = self.BND2
+        if self.returns(self.BND1, self.RET, self.EXCL) < self.returns(self.BND2, self.RET, self.EXCL):
+            self.selected_bond = self.BND2
             
-        elif self.Returns(self.BND1, self.mom, self.excl) > self.Returns(self.BND2, self.mom, self.excl):
-            self.BNDselect = self.BND1
+        elif self.returns(self.BND1, self.RET, self.EXCL) > self.returns(self.BND2, self.RET, self.EXCL):
+            self.selected_bond = self.BND1
             
-        if self.Returns(self.STK1, self.mom, self.excl) < self.Returns(self.STK2, self.mom, self.excl):
-            self.STKselect =  self.STK2
+        if self.returns(self.STK1, self.RET, self.EXCL) < self.returns(self.STK2, self.RET, self.EXCL):
+            self.selected_stock = self.STK2
             
-        elif self.Returns(self.STK1, self.mom, self.excl) > self.Returns(self.STK2, self.mom, self.excl):
-            self.STKselect =  self.STK1
+        elif self.returns(self.STK1, self.RET, self.EXCL) > self.returns(self.STK2, self.RET, self.EXCL):
+            self.selected_stock = self.STK1
             
-        self.HLD_IN = {self.STKselect: 1}
-        self.HLD_OUT = {self.BNDselect: 1}
-            
-
-    def rebalance_when_out_of_the_market(self):
+                    
+    def trade_out(self):
+        
         if not self.bull:
-            self.trade({**dict.fromkeys(self.HLD_IN, 0), **self.HLD_OUT})
+            for sec in self.ASSETS:    
+                self.wt[sec] = 0.99 if sec is self.selected_bond else 0 if sec is self.selected_bond else 0
+            self.trade() 
             
-
-    def rebalance_when_in_the_market(self):
-        if self.bull:
-            self.trade({**self.HLD_IN, **dict.fromkeys(self.HLD_OUT, 0)})
-            self.Log(f"TotalPortfolioValue: {self.Portfolio.TotalPortfolioValue}, TotalMarginUsed: {self.Portfolio.TotalMarginUsed}, MarginRemaining: {self.Portfolio.MarginRemaining}, Cash:  {self.Portfolio.Cash}")
-            for key in sorted(self.Portfolio.keys()):
-                if self.Portfolio[key].Quantity > 0.0:
-                    self.Log(f"Symbol/Qty: {key} / {self.Portfolio[key].Quantity}, Avg: {self.Portfolio[key].AveragePrice}, Curr: { self.Portfolio[key].Price}, Profit($): {self.Portfolio[key].UnrealizedProfit}")
-
-
-    def trade(self, weight_by_sec):
-        if self.Portfolio.Invested:
-            for symbol in self.Portfolio.Keys:
-                if symbol not in weight_by_sec:
-                    self.Liquidate(symbol)
             
-        buys = []
-        for sec, weight in weight_by_sec.items():
-            if not self.CurrentSlice.ContainsKey(sec) or self.CurrentSlice[sec] is None:
-                continue
-            
+    def trade_in(self):
+        
+        if self.bull:    
+            for sec in self.ASSETS:
+                self.wt[sec] = 0.99 if sec is self.selected_stock else 0
+            self.trade()            
+
+                    
+    def trade(self):
+
+        for sec, weight in self.wt.items():
+            if weight == 0 and self.Portfolio[sec].IsLong:
+                self.Liquidate(sec)
+                
             cond1 = weight == 0 and self.Portfolio[sec].IsLong
             cond2 = weight > 0 and not self.Portfolio[sec].Invested
             if cond1 or cond2:
-                quantity = self.CalculateOrderQuantity(sec, weight)
-                if quantity > 0:
-                    buys.append((sec, quantity))
-                elif quantity < 0:
-                    self.Order(sec, quantity)
-        for sec, quantity in buys:
-            self.Order(sec, quantity)
+                self.SetHoldings(sec, weight)
             
                     
     def record_vars(self):                
@@ -192,3 +184,6 @@ class DualMomentumInOut(QCAlgorithm):
         
         account_leverage = self.Portfolio.TotalHoldingsValue / self.Portfolio.TotalPortfolioValue
         self.Plot('Holdings', 'leverage', round(account_leverage, 1))
+        for sec, weight in self.wt.items(): 
+            self.real_wt[sec] = round(self.ActiveSecurities[sec].Holdings.Quantity * self.Securities[sec].Price / self.Portfolio.TotalPortfolioValue,4)
+            self.Plot('Holdings', self.Securities[sec].Symbol, round(self.real_wt[sec], 3))
